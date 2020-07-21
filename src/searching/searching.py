cb8e571997daefd2847e0b26d70ff09cb0abd8b6
Jiayi Ren <jiayiren22@gmail.com>
2020-07-21 10:56:26 -0700

    complete MVP: recursive binary search, merge sort

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,18 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
-
+    if len(arr) == 0:
+        return -1
+    if start <= end:
+        pivot = start + (end - start) //2
+        if arr[pivot] == target:
+            return pivot
+        elif arr[pivot] > target:
+            return binary_search(arr, target, start, pivot)
+        elif arr[pivot] < target:
+            return binary_search(arr, target, pivot, end)
+    else:
+        return -1
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -4,14 +4,38 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
-
+    curr = 0
+    while len(arrA) > 0 and len(arrB) > 0:
+        if arrA[0] <= arrB[0]:
+            merged_arr[curr] = arrA.pop(0)
+            curr += 1
+        else:
+            merged_arr[curr] = arrB.pop(0)
+            curr += 1
+    if len(arrA) > 0:
+        for i in arrA:
+            merged_arr[curr] = i
+            curr += 1
+    if len(arrB) > 0:
+        for i in arrB:
+            merged_arr[curr] = i
+            curr += 1
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr)//2
 
+    left_array = arr[:mid]
+    right_array = arr[mid:]
+    left_array = merge_sort(left_array)
+    right_array = merge_sort(right_array)
+
+    arr = merge(left_array, right_array)
 
     return arr
 
